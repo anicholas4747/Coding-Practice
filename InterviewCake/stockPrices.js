f3a9ca29d20aba1c94b52bce5fb8d5e800fe909f
anicholas4747 <an1720@nyu.edu>
2020-03-24 18:18:00 -0400

    starting w interview cake

---
+++ InterviewCake/stockPrices.js
@@ -0,0 +1,66 @@
+function getMaxProfit(stockPrices) {
+
+  if (stockPrices.length < 2) throw "WTF"
+
+  // Calculate the max profit
+  let maxP = 0;
+  for (let i = 0; i < stockPrices.length; i++) {
+    for (let j = i + 1; j < stockPrices.length; j++) {
+      let thisP = (stockPrices[j] - stockPrices[i]);
+      if ((i === 0 && j === 1) || thisP > maxP) maxP = thisP;
+    }
+  }
+  return maxP;
+}
+
+// Tests
+
+let desc = 'price goes up then down';
+let actual = getMaxProfit([1, 5, 3, 2]);
+let expected = 4;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes down then up';
+actual = getMaxProfit([7, 2, 8, 9]);
+expected = 7;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes up all day';
+actual = getMaxProfit([1, 6, 7, 9]);
+expected = 8;
+assertEqual(actual, expected, desc);
+
+desc = 'price goes down all day';
+actual = getMaxProfit([9, 7, 4, 1]);
+expected = -2;
+assertEqual(actual, expected, desc);
+
+desc = 'price stays the same all day';
+actual = getMaxProfit([1, 1, 1, 1]);
+expected = 0;
+assertEqual(actual, expected, desc);
+
+desc = 'error with empty prices';
+const emptyArray = () => (getMaxProfit([]));
+assertThrowsError(emptyArray, desc);
+
+desc = 'error with one price';
+const onePrice = () => (getMaxProfit([1]));
+assertThrowsError(onePrice, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`);
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}

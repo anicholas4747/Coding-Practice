0e64ccc941cf70d9228684d72ec9e62727d9f130
anicholas4747 <an1720@nyu.edu>
2020-04-09 12:43:20 -0400

    38

---
+++ LeetCode/1easy/38.js
@@ -0,0 +1,60 @@
+// 38. Count and Say
+// Easy
+
+// The count - and - say sequence is the sequence of integers with the first five terms as following:
+
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
+// 1 is read off as "one 1" or 11.
+// 11 is read off as "two 1s" or 21.
+// 21 is read off as "one 2, then one 1" or 1211.
+
+// Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count - and - say sequence.You can do so recursively, in other words from the previous member read off the digits, counting the number of digits in groups of the same digit.
+
+//   Note: Each term of the sequence of integers will be represented as a string.
+
+
+
+//     Example 1:
+
+// Input: 1
+// Output: "1"
+// Explanation: This is the base case.
+// Example 2:
+
+// Input: 4
+// Output: "1211"
+// Explanation: For n = 3 the term was "21" in which we have two groups "2" and "1", "2" can be read as "12" which means frequency = 1 and value = 2, the same way "1" is read as "11", so the answer is the concatenation of "12" and "11" which is "1211".
+
+var countAndSay = function (n) {
+
+  // base case = n = 1 & 2
+  if (n === 1) return "1";
+  if (n === 2) return "11";
+
+  // check prev result
+  let lastSeq = countAndSay(n - 1);
+
+  // generate the str
+  let newStr = "";
+  let counter = 0;
+  for (let i = 0; i < lastSeq.length; i++) {
+    let char = lastSeq[i];
+    let nextChar = lastSeq[i + 1];
+    counter++;
+    if (nextChar && char === nextChar) {
+      continue;
+    } else {
+      newStr += `${counter}${char}`;
+      counter = 0;
+    }
+  }
+  return newStr;
+};
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`countAndSay(${i}) --- `,countAndSay(i));
+}
